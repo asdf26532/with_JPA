63895840936f14c9a2537dd3c86d94140d1ea443
SeungHwan Lee <asdf26532@gmail.com>
2024-06-12 21:41:59 +0900

    connection test

--- src/main/java/com/shop/study/StudyApplication.java
+++ src/main/java/com/shop/study/StudyApplication.java
@@ -2,7 +2,10 @@ package com.shop.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class StudyApplication {
 
@@ -10,4 +13,8 @@ public class StudyApplication {
 		SpringApplication.run(StudyApplication.class, args);
 	}
 
+	@GetMapping(value = "/")
+	public String HelloWorld() {
+		return "Hello World";
+	}
 }
---
+++ src/main/java/com/shop/study/UserDto.java
@@ -0,0 +1,14 @@
+package com.shop.study;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class UserDto {
+    private String name;
+    private int age;
+
+}
