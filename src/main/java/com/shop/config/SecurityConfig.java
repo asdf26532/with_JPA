6251aef248051bcdd9eeaffffcd511948108ca22
SeungHwan Lee <asdf26532@gmail.com>
2024-06-25 22:07:32 +0900

    member update

---
+++ src/main/java/com/shop/config/SecurityConfig.java
@@ -0,0 +1,14 @@
+package com.shop.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    memberser
+}
